98f8eb4d1d580a4d4ff8119bb1ea95fe8eae4add
DerHumpink <steinbock@threaks.com>
2015-11-06 13:58:16 +0100

    Moved GameForm to GameForm namespace

---
+++ Setup.exe/GameForms/GameForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Setup.exe.GameForms
+{
+	public class GameForm : Form
+	{
+		private Thread _updateThread;
+		private readonly Action _updateAction;
+		private volatile bool _shouldStop;
+
+		public GameForm()
+		{
+			_updateAction=Update;
+		}
+
+		private void SetupUpdateThread()
+		{
+			_updateThread = new Thread(DoUpdate);
+			_updateThread.Start();
+		}
+
+		private void DoUpdate()
+		{
+			while (!_shouldStop)
+			{
+				try
+				{
+					Invoke(_updateAction);
+				}
+				catch
+				{
+					Console.WriteLine("Exeption caught in Update Action");
+				}
+				Thread.Sleep(16);
+			}
+		}
+
+		public virtual void OnGameUpdate()
+		{
+			
+		}
+
+		protected override void OnFormClosed(FormClosedEventArgs e)
+		{
+			StopUpdateThread();
+			base.OnFormClosed(e);
+		}
+
+		private void StopUpdateThread()
+		{
+			_shouldStop = true;
+			_updateThread.Join();
+		}
+	}
+}
